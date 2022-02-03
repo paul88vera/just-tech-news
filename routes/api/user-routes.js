a0664a5d11a2ca18faa0303ae815c846481dab45
paul88vera <paul88vera@gmail.com>
2022-02-03 13:58:13 -0600

    passwords are hashed

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,7 +5,7 @@ const { User } = require("../../models");
 router.get("/", (req, res) => {
   // Access our User model and run .findAll() method)
   User.findAll({
-    attributes: { exclude: ["password"] },
+    // attributes: { exclude: ["password"] },
   })
     .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
@@ -17,7 +17,7 @@ router.get("/", (req, res) => {
 // GET /api/users/1
 router.get("/:id", (req, res) => {
   User.findOne({
-    attributes: { exclude: ["password"]},
+    // attributes: { exclude: ["password"]},
     where: {
       id: req.params.id,
     },
@@ -56,6 +56,7 @@ router.put("/:id", (req, res) => {
 
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id,
     },
