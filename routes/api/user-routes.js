2fa3f2ac3fba1663c3dd9b882260410d9c29de99
paul88vera <paul88vera@gmail.com>
2022-02-04 00:04:29 -0600

    issue #4 completed

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Post, Vote } = require("../../models");
 
-// GET /api/users
+// GET all users
 router.get("/", (req, res) => {
   // Access our User model and run .findAll() method)
   User.findAll({
-    // attributes: { exclude: ["password"] },
+    attributes: { exclude: ["password"] },
   })
     .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
@@ -14,10 +14,22 @@ router.get("/", (req, res) => {
     });
 });
 
-// GET /api/users/1
+// GET users/1
 router.get("/:id", (req, res) => {
   User.findOne({
-    // attributes: { exclude: ["password"]},
+    attributes: { exclude: ["password"] },
+    include: [
+      {
+        model: Post,
+        attributes: ["id", "title", "post_url", "created_at"],
+      },
+      {
+        model: Post,
+        attributes: ["title"],
+        through: Vote,
+        as: "voted_posts",
+      },
+    ],
     where: {
       id: req.params.id,
     },
@@ -50,24 +62,24 @@ router.post("/", (req, res) => {
     });
 });
 
-router.post('/login', (req,res) => {
+router.post("/login", (req, res) => {
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
-      email: req.body.email
-    }
-  }).then(dbUserData => {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
     if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
+      res.status(400).json({ message: "No user with that email address!" });
       return;
     }
     // Verify user
     const validPassword = dbUserData.checkPassword(req.body.password);
     if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
+      res.status(400).json({ message: "Incorrect password!" });
       return;
     }
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
+    res.json({ user: dbUserData, message: "You are now logged in!" });
   });
 });
 
